b6ea3138bbab5c8b7a421e0f70d154b40c0c7c07
Tarik Sabin <tarik.sabin@gmail.com>
2019-12-01 19:44:11 +0000

    3 down 3 to go.

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -9,49 +9,84 @@ namespace comp110_worksheet_7
 {
 	public static class DirectoryUtils
 	{
-		// Return the size, in bytes, of the given file
-		public static long GetFileSize(string filePath)
+        // Declare array to store file names.
+        public static string[] FileArr;
+
+        // Return the size, in bytes, of the given file.
+        public static long GetFileSize(string filePath)
 		{
 			return new FileInfo(filePath).Length;
 		}
 
-		// Return true if the given path points to a directory, false if it points to a file
+		// Return true if the given path points to a directory, false if it points to a file.
 		public static bool IsDirectory(string path)
 		{
 			return File.GetAttributes(path).HasFlag(FileAttributes.Directory);
 		}
 
-		// Return the total size, in bytes, of all the files below the given directory
+		// Return the total size, in bytes, of all the files below the given directory.
 		public static long GetTotalSize(string directory)
 		{
-			throw new NotImplementedException();
+            //  Declare size variable.
+            long sizeSum = 0;
+
+            // Add files to the array. *.* so only files will be added and not folders.
+            FileArr = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            // Get the size of each file in the array and add its size to the sum.
+            foreach (string fileName in FileArr)
+            {
+                sizeSum = sizeSum + GetFileSize(fileName);
+            }
+
+            // Return total size.
+            return sizeSum;
 		}
 
-		// Return the number of files (not counting directories) below the given directory
+		// Return the number of files (not counting directories) below the given directory.
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            // Add files to the array. *.* so only files will be added and not folders.
+            FileArr = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            // Return the amount of items in the array.
+            return FileArr.Length;
+        }
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            // Array for directories.
+            string[] Directories;
+            // Declare count variable.
+            int depthCount = 0;
 
-		// Get the path and size (in bytes) of the smallest file below the given directory
+            // Add directories to the array.
+            Directories = Directory.GetFiles(directory);
+
+            // Increment for each aditional depth.
+            foreach (string dir in Directories)
+            {
+                depthCount++;
+            }
+
+            // Return the depth.
+            return depthCount;
+        }
+
+		// Get the path and size (in bytes) of the smallest file below the given directory.
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
 			throw new NotImplementedException();
 		}
 
-		// Get the path and size (in bytes) of the largest file below the given directory
+		// Get the path and size (in bytes) of the largest file below the given directory.
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
 			throw new NotImplementedException();
 		}
 
-		// Get all files whose size is equal to the given value (in bytes) below the given directory
+		// Get all files whose size is equal to the given value (in bytes) below the given directory.
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
 			throw new NotImplementedException();
